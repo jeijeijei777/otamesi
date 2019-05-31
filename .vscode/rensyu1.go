78bc30d5bd4513bec4ac981fce79040fca99dbac
user <hiroaking717@gmail.com>
2019-05-31 18:49:36 +0900

    commit

--- .vscode/rensyu1.go
+++ .vscode/rensyu1.go
@@ -1,7 +1,27 @@
 package main
 
-import fmt "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
 
 func main() {
-	fmt.Printf("こんにちは")
+	fmt.Println("My favorite number is", rand.Intn(10))
+
+	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+	fmt.Println(math.Pi)
+
+	fmt.Println(add(12, 32))
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
